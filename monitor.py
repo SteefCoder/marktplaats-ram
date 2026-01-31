dbcec96ee25ce553fd3f4071cceb65316e03ed19
SteefCoder <stefanvanderduijs@icloud.com>
2026-01-31 22:23:15 +0100

    Relaxed intervals again

--- monitor.py
+++ monitor.py
@@ -19,13 +19,13 @@ logging.Formatter.converter = lambda *args: dates.now().timetuple()
 # tuples with how many days back we want to get the listings
 # and in how many hours we want to check for that
 # since today - every 2 hours
-# since yesterday - every 4 hours
+# since yesterday - every 6 hours
 # since the last week - every day
 # since the last month - every 2 days
 # since the last year - every 4 days
 UPDATE_INTERVAL_HOURS = [
-    (timedelta(days=0), 1),
-    (timedelta(days=1), 3),
+    (timedelta(days=0), 2),
+    (timedelta(days=1), 6),
     (timedelta(days=7), 24),
     (timedelta(days=30), 48),
     (timedelta(days=365), 96)
